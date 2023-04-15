615f44cdf0e85be912b5614d4e3048f9d382ea1c
CollyLee <clh5160@gmail.com>
2023-04-15 12:14:32 -0400

    wrote questions and began pseudocoding

---
+++ index.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+const 
+
+const questions = [
+    {
+        type: 'input',
+        message: `Enter the text you'd like on your logo (three chars max).`,
+        name: 'logoChars',
+    },
+    {
+        type: 'input',
+        message: 'Enter a text color (name or hex number).',
+        name: 'textColor',
+    },
+    {
+        type: 'list',
+        message: `Choose the shape you'd like for your logo.`,
+        name: 'logoShape',
+        choices: [`Circle`, `Triangle`, `Square`]
+    },
+    {
+        type: 'input',
+        message: 'Enter a color fo the shape (name or hex number).',
+        name: 'shapeColor',
+    },
+]
+
+// const createLogo(data) {
+    // create a variable that is the data from the generate logo file (logoData(data))
+    // fs.writeFile('logo.svg', logoData, (err) => 
+    // err? console.error(err) " console.log('Your logo is ready! Check the root folder for the new file.")
+//     )
+// }
+
+const init = function () {
+    inquirer.prompt(questions)
+        .then(data) =>
+createFile(data)
+
+}
+
+init()
